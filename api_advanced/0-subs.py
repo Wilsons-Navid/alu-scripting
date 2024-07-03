718eecdc1ce21131d9836fc09c11580c9374695e
Wilsons-Navid <wadotiwawil@gmail.com>
2024-07-03 07:57:51 +0000

    k

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""Return the number of subscribers of a given subreddit"""
-
+"""
+Python script that returns the number of subscribers
+    for a given subreddit
+"""
 import requests
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """function that fetches number_of_subscribers"""
-    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    """
+    Function that queries the Reddit API and returns the number of subscribers
+    for a given subreddit
+    """
+    url = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit=subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    return 0
 
-    try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        return RESPONSE.json().get("data").get("subscribers")
 
-    except Exception:
-        return 0
+if __name__ == "__main__":
+    subreddit = argv[1]
+    print(number_of_subscribers(subreddit))
