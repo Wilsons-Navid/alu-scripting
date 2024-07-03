861324f2a6e47b47cf67b68f133d3d952e210ddf
Wilsons-Navid <wadotiwawil@gmail.com>
2024-07-03 06:52:09 +0000

    lk

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 """
-function that queries the 'Reddit API' and returns the number of subscribers
+Python script that returns the number of subscribers
+    for a given subreddit
 """
 import requests
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
     """
-    number of subscribers
+    Function that queries the Reddit API and returns the number of subscribers
+    for a given subreddit
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}  # avoid Too Many Requests error
+    url = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit=subreddit)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 200:
-        data = response.json()["data"]
-        return data["subscribers"]
-    else:
-        return OK
+        return response.json().get("data").get("subscribers")
+    return 0
+
+
+if __name__ == "__main__":
+    subreddit = argv[1]
+    print(number_of_subscribers(subreddit))
